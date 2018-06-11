c560a639895897ee0418631a76de8181b5e52a66
Patricio Soriano <pasoriano@gmail.com>
2018-06-11 19:35:01 +0200

    museos

---
+++ mapea/museos/sv22_museo.js
@@ -0,0 +1,12 @@
+var geo = {
+"type": "FeatureCollection",
+"name": "sv22_museo",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::25830" } },
+"features": [
+{ "type": "Feature", "properties": { "nomb_mus": "Museo Regina", "municipio": "Córdoba", "cod_mun": "14021", "provincia": "Córdoba", "cod_ent": "s74" }, "geometry": { "type": "Point", "coordinates": [ 344145.673822172335349, 4194459.95224514324218 ] } },
+{ "type": "Feature", "properties": { "nomb_mus": "Museo Julio Romero de Torres", "municipio": "Córdoba", "cod_mun": "14021", "provincia": "Córdoba", "cod_ent": "s74" }, "geometry": { "type": "Point", "coordinates": [ 343917.67187816335354, 4194121.953487528953701 ] } },
+{ "type": "Feature", "properties": { "nomb_mus": "Museo Arqueológico y Etnológico de Córdoba", "municipio": "Córdoba", "cod_mun": "14021", "provincia": "Córdoba", "cod_ent": "s74" }, "geometry": { "type": "Point", "coordinates": [ 343642.673265948542394, 4194201.95453968597576 ] } },
+{ "type": "Feature", "properties": { "nomb_mus": "Museo de Bellas Artes de Córdoba", "municipio": "Córdoba", "cod_mun": "14021", "provincia": "Córdoba", "cod_ent": "s74" }, "geometry": { "type": "Point", "coordinates": [ 343917.67187816335354, 4194121.953487528953701 ] } },
+{ "type": "Feature", "properties": { "nomb_mus": "Museo Torre de la Calahorra", "municipio": "Córdoba", "cod_mun": "14021", "provincia": "Córdoba", "cod_ent": "s74" }, "geometry": { "type": "Point", "coordinates": [ 343751.667580443550833, 4193502.954770668875426 ] } }
+]
+}
