1f8ca38a0949c7be985a621bd3bd88ed79b653b5
Patricio Soriano <pasoriano@gmail.com>
2018-06-11 19:53:57 +0200

    viales cdau

---
+++ mapea/viales/opengeocordoba.js
@@ -0,0 +1,78 @@
+mapajs = M.map({
+    //container: "map",controls:['Scale','ScaleLine','layerswitcher','location','mouse'],
+    container: "map",controls:["panzoombar",'Scale','ScaleLine','location','mouse','layerswitcher'],
+    center: {
+      x: 350373,
+      y: 4190372
+    },
+    zoom: 7,
+    wmcfiles: ["cdau_satelite","cdau"]
+
+  });
+
+
+let estiloPol = new M.style.Polygon({
+   // fill: {
+   //   color: 'pink',
+   //   opacity: 0.5,
+   // },
+   stroke: {
+   	color: '#FF0000',
+   	width: 2
+   }
+ });
+
+var layerTM = new M.layer.WFS({
+	url: "http://www.ideandalucia.es/services/dera_div_admin/wfs?",
+  // namespace: "tematicos",
+  name: "dera_div_admin:da02_term_munic",
+  legend: "TM",
+  geometry: 'POLYGON',
+  extract: true
+  //ids:"3,4"
+},{
+	vendor:{
+		getFeature: {
+			'filter': '<Filter><PropertyIsEqualTo><PropertyName>cod_mun</PropertyName><Literal>14021</Literal></PropertyIsEqualTo></Filter>'
+		}
+	}
+});
+layerTM.setStyle(estiloPol);
+mapajs.addWFS(layerTM);
+
+
+
+
+let estiloBlue = new M.style.Point({
+	radius: 5, 
+	fill: {  
+		color: 'blue'
+     //opacity: 0.5
+   },
+   stroke: {
+  	color: '#FFFFFF',   // Color del borde. Hexadecimal, nominal
+     width: 2           // Grosor en pixeles
+   }
+
+ });
+
+// linea amarilla de grosor dos
+let estiloLinAmarilla = new M.style.Line({
+    fill: {
+       color: 'yellow',
+       width: 2
+    }
+});
+
+
+
+
+var layercdau_viales = new M.layer.GeoJSON(
+	{name: "Viales CDAU", 
+	source: geo
+
+});
+
+layercdau_viales.setStyle(estiloLinAmarilla );
+mapajs.addLayers(layercdau_viales);
+
